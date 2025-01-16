682828ae9fb3ec2fd7fce4eeeb2aa887b6a57888
Danny Keig <39764493+dannyZyg@users.noreply.github.com>
2025-01-16 12:01:27 +1100

    Query the vocab db for lookups from sentences and show them in a table

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-import json
 import os
 import platform
 import sqlite3
 import threading
 import time
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from jinja2 import Environment, FileSystemLoader
+from db import KindleVocabDB
 
-from http.server import BaseHTTPRequestHandler, HTTPServer
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates_env = Environment(loader=FileSystemLoader("templates"))
 
 # What we expect the Kindle to be named when it is mounted
 VALID_KINDLE_MOUNTS = [
     "Kindle",
-    "NONAME",
+    "NO NAME",
 ]
 
 # We can add more here later as we discover them
@@ -22,34 +32,32 @@ PORT = 11000
 db = None
 
 
-class KindleVocabServer(BaseHTTPRequestHandler):
-    def _set_headers(self):
-        self.send_response(200)
-        self.send_header("Content-Type", "application/json")
-        self.end_headers()
+def shutdown_server():
+    print("Exiting...")
+    os._exit(0)
 
-    # Just blindly allow any request to / to get the WORDS table
-    # in a JSON format
-    def do_GET(self):
-        global db
-        cursor = db.cursor()
-        try:
-            results = cursor.execute("SELECT * FROM WORDS").fetchall()
-        except sqlite3.DatabaseError:
-            print(
-                "Encountered database error during query, assuming Kindle has been unplugged so returning to wait mode."
-            )
-            # This will typically happen when the Kindle gets unplugged
-            # We have to tell the server to shutdown in a separate thread
-            # as this request is happening inside the server thread, and
-            # so would get deadlocked.
-            threading.Thread(target=self.server.shutdown).start()
-        else:
-            # If no exception has occurred, return the results
-            data = [dict(row) for row in results]
 
-            self._set_headers()
-            self.wfile.write(json.dumps(data).encode("utf-8"))
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    global db
+
+    try:
+        results = db.get_lookups()
+    except sqlite3.DatabaseError:
+        print(
+            "Encountered database error during query, assuming Kindle has been unplugged so returning to wait mode."
+        )
+        # This will typically happen when the Kindle gets unplugged
+        # We have to tell the server to shutdown in a separate thread
+        # as this request is happening inside the server thread, and
+        # so would get deadlocked.
+
+        threading.Thread(target=shutdown_server).start()
+    else:
+        # If no exception has occurred, return the results
+        template = templates_env.get_template("index.html")
+        html_content = template.render(lookups=results)
+        return HTMLResponse(content=html_content)
 
 
 def wait_for_kindle():
@@ -88,11 +96,14 @@ def serve_db(vocab_db_path):
     global db
     db_uri = f"file:{vocab_db_path}?mode=ro"
 
-    db = sqlite3.connect(db_uri, uri=True)
-    db.row_factory = sqlite3.Row
+    db = KindleVocabDB(db_uri)
+    config = uvicorn.Config(app=app, host="127.0.0.1", port=PORT)
+    server = uvicorn.Server(config)
 
-    httpd = HTTPServer(("", PORT), KindleVocabServer)
-    httpd.serve_forever()
+    try:
+        server.run()
+    except KeyboardInterrupt:
+        shutdown_server()
 
 
 if __name__ == "__main__":
